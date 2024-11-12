98279fe0910096e520255c6cbdca6f97d24d359c
Ennio Marke <mystical.ennio@gmail.com>
2024-11-12 15:09:42 +0100

    tileSize

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let padding = 10;
-let desiredTileSize = 30;
+let desiredTileSize =
+    URLSearchParams(window.location.search).get('tileSize') || 30;
 let canvasHeight = window.innerHeight / 1.5;
 let canvasWidth = window.innerWidth - padding * 2;
 let gridRows = Math.round(canvasWidth / desiredTileSize);
@@ -7,7 +8,7 @@ let gridRowWidth = canvasWidth / gridRows;
 let gridCols = Math.floor(canvasHeight / gridRowWidth);
 let gridColWidth = canvasHeight / gridCols;
 
-let cv = new canvas("gameCanvas", canvasWidth, canvasHeight, "white");
+let cv = new canvas('gameCanvas', canvasWidth, canvasHeight, 'white');
 
 let gm = new gameManager(cv, gridRows, gridCols);
 
@@ -16,9 +17,9 @@ let gameSpeed = 500;
 function resetGame() {
     gm.reset();
     gm.pause();
-    playPauseButton.children[0].classList.remove("hidden");
-    playPauseButton.children[1].classList.add("hidden");
-    playPauseButton.children[2].innerHTML = "Play";
+    playPauseButton.children[0].classList.remove('hidden');
+    playPauseButton.children[1].classList.add('hidden');
+    playPauseButton.children[2].innerHTML = 'Play';
     gm.cell(5, 5);
     gm.cell(6, 5);
     gm.cell(7, 5);
@@ -26,21 +27,21 @@ function resetGame() {
     gm.cell(6, 3);
 }
 
-let playPauseButton = document.getElementById("playPauseButton");
+let playPauseButton = document.getElementById('playPauseButton');
 
 function togglePlayPause() {
-    gameSpeed = Math.abs(document.getElementById("speedInput").value);
+    gameSpeed = Math.abs(document.getElementById('speedInput').value);
     if (gm.playing) {
         gm.pause();
-        playPauseButton.children[0].classList.remove("hidden");
-        playPauseButton.children[1].classList.add("hidden");
-        playPauseButton.children[2].innerHTML = "Play";
+        playPauseButton.children[0].classList.remove('hidden');
+        playPauseButton.children[1].classList.add('hidden');
+        playPauseButton.children[2].innerHTML = 'Play';
         return;
     }
     gm.play(gameSpeed);
-    playPauseButton.children[0].classList.add("hidden");
-    playPauseButton.children[1].classList.remove("hidden");
-    playPauseButton.children[2].innerHTML = "Pause";
+    playPauseButton.children[0].classList.add('hidden');
+    playPauseButton.children[1].classList.remove('hidden');
+    playPauseButton.children[2].innerHTML = 'Pause';
 }
 
 resetGame();
